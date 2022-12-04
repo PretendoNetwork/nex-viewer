d898775c50d6f9926466d97e99807101c31ce04c
Jonathan Barrow <jonbarrow1998@gmail.com>
2022-12-04 18:26:14 -0500

    Added toJSON function to all Friends WiiU types

--- src/protocols/types/friends_wiiu.js
+++ src/protocols/types/friends_wiiu.js
@@ -17,6 +17,27 @@ class NNAInfo extends NEXTypes.Structure {
 		this.unknown1 = stream.readUInt8();
 		this.unknown2 = stream.readUInt8();
 	}
+
+	toJSON() {
+		return {
+			__typeInherits: this._parentTypes.map(value => ({
+				__typeName: value.constructor.name,
+				__typeValue: value
+			})),
+			principalBasicInfo: {
+				__typeName: 'PrincipalBasicInfo',
+				__typeValue: this.principalBasicInfo
+			},
+			unknown1: {
+				__typeName: 'uint8',
+				__typeValue: this.unknown1
+			},
+			unknown2: {
+				__typeName: 'uint8',
+				__typeValue: this.unknown2
+			}
+		};
+	}
 }
 NEXTypes.AnyDataHolder.addType('NNAInfo', NNAInfo);
 
@@ -37,6 +58,31 @@ class PrincipalBasicInfo extends NEXTypes.Structure {
 		this.mii = stream.readNEXStructure(MiiV2);
 		this.unknown2 = stream.readUInt8();
 	}
+
+	toJSON() {
+		return {
+			__typeInherits: this._parentTypes.map(value => ({
+				__typeName: value.constructor.name,
+				__typeValue: value
+			})),
+			pid: {
+				__typeName: 'PID',
+				__typeValue: this.pid
+			},
+			nnid: {
+				__typeName: 'String',
+				__typeValue: this.nnid
+			},
+			mii: {
+				__typeName: 'MiiV2',
+				__typeValue: this.mii
+			},
+			unknown2: {
+				__typeName: 'uint8',
+				__typeValue: this.unknown2
+			}
+		};
+	}
 }
 
 class MiiV2 extends NEXTypes.Structure {
@@ -56,6 +102,35 @@ class MiiV2 extends NEXTypes.Structure {
 		this.unknown2 = stream.readUInt8();
 		this.data = stream.readNEXBuffer();
 		this.unknown3 = stream.readNEXDateTime();
+	}
+
+	toJSON() {
+		return {
+			__typeInherits: this._parentTypes.map(value => ({
+				__typeName: value.constructor.name,
+				__typeValue: value
+			})),
+			name: {
+				__typeName: 'String',
+				__typeValue: this.name
+			},
+			unknown1: {
+				__typeName: 'uint8',
+				__typeValue: this.unknown1
+			},
+			unknown2: {
+				__typeName: 'uint8',
+				__typeValue: this.unknown2
+			},
+			data: {
+				__typeName: 'Buffer',
+				__typeValue: this.data
+			},
+			unknown3: {
+				__typeName: 'DateTime',
+				__typeValue: this.unknown3
+			}
+		};
 	}
 }
 
@@ -87,6 +162,75 @@ class NintendoPresenceV2 extends NEXTypes.Structure {
 		this.unknown6 = stream.readUInt8();
 		this.unknown7 = stream.readUInt8();
 	}
+
+	toJSON() {
+		return {
+			__typeInherits: this._parentTypes.map(value => ({
+				__typeName: value.constructor.name,
+				__typeValue: value
+			})),
+			changedFlags: {
+				__typeName: 'uint32',
+				__typeValue: this.changedFlags
+			},
+			isOnline: {
+				__typeName: 'boolean',
+				__typeValue: this.isOnline
+			},
+			gameKey: {
+				__typeName: 'GameKey',
+				__typeValue: this.gameKey
+			},
+			unknown1: {
+				__typeName: 'uint8',
+				__typeValue: this.unknown1
+			},
+			message: {
+				__typeName: 'String',
+				__typeValue: this.message
+			},
+			unknown2: {
+				__typeName: 'uint32',
+				__typeValue: this.unknown2
+			},
+			unknown3: {
+				__typeName: 'uint8',
+				__typeValue: this.unknown3
+			},
+			gameServerId: {
+				__typeName: 'uint32',
+				__typeValue: this.gameServerId
+			},
+			unknown4: {
+				__typeName: 'uint32',
+				__typeValue: this.unknown4
+			},
+			pid: {
+				__typeName: 'PID',
+				__typeValue: this.pid
+			},
+			gatheringId: {
+				__typeName: 'uint32',
+				__typeValue: this.gatheringId
+			},
+			applicationData: {
+				__typeName: 'Buffer',
+				__typeValue: this.applicationData
+			},
+			unknown5: {
+				__typeName: 'uint8',
+				__typeValue: this.unknown5
+			},
+			unknown6: {
+				__typeName: 'uint8',
+				__typeValue: this.unknown6
+			},
+			unknown7: {
+				__typeName: 'uint8',
+				__typeValue: this.unknown7
+			}
+		};
+	}
 }
 NEXTypes.AnyDataHolder.addType('NintendoPresenceV2', NintendoPresenceV2);
 
@@ -104,6 +248,23 @@ class GameKey extends NEXTypes.Structure {
 	parse(stream) {
 		this.titleId = stream.readUInt64LE();
 		this.titleVersion = stream.readUInt16LE();
+	}
+
+	toJSON() {
+		return {
+			__typeInherits: this._parentTypes.map(value => ({
+				__typeName: value.constructor.name,
+				__typeValue: value
+			})),
+			titleId: {
+				__typeName: 'uint64',
+				__typeValue: this.titleId
+			},
+			titleVersion: {
+				__typeName: 'uint16',
+				__typeValue: this.titleVersion
+			}
+		};
 	}
 }
 
@@ -123,6 +284,27 @@ class PrincipalPreference extends NEXTypes.Structure {
 		this.showCurrentlyPlayingTitle = stream.readBoolean();
 		this.blockFriendRequests = stream.readBoolean();
 	}
+
+	toJSON() {
+		return {
+			__typeInherits: this._parentTypes.map(value => ({
+				__typeName: value.constructor.name,
+				__typeValue: value
+			})),
+			showOnlinePresence: {
+				__typeName: 'boolean',
+				__typeValue: this.showOnlinePresence
+			},
+			showCurrentlyPlayingTitle: {
+				__typeName: 'boolean',
+				__typeValue: this.showCurrentlyPlayingTitle
+			},
+			blockFriendRequests: {
+				__typeName: 'boolean',
+				__typeValue: this.blockFriendRequests
+			}
+		};
+	}
 }
 NEXTypes.AnyDataHolder.addType('PrincipalPreference', PrincipalPreference);
 
@@ -138,9 +320,30 @@ class Comment extends NEXTypes.Structure {
 	 * @param {Stream} stream NEX data stream
 	 */
 	parse(stream) {
-		this.Unknown = stream.readUInt8();
+		this.unknown = stream.readUInt8();
 		this.statusMessage = stream.readNEXString();
 		this.lastChangedOn = stream.readNEXDateTime();
+	}
+
+	toJSON() {
+		return {
+			__typeInherits: this._parentTypes.map(value => ({
+				__typeName: value.constructor.name,
+				__typeValue: value
+			})),
+			unknown: {
+				__typeName: 'uint8',
+				__typeValue: this.unknown
+			},
+			statusMessage: {
+				__typeName: 'String',
+				__typeValue: this.statusMessage
+			},
+			lastChangedOn: {
+				__typeName: 'DateTime',
+				__typeValue: this.lastChangedOn
+			}
+		};
 	}
 }
 
@@ -163,6 +366,39 @@ class FriendInfo extends NEXTypes.Structure {
 		this.lastOnline = stream.readNEXDateTime();
 		this.unknown = stream.readUInt64LE();
 	}
+
+	toJSON() {
+		return {
+			__typeInherits: this._parentTypes.map(value => ({
+				__typeName: value.constructor.name,
+				__typeValue: value
+			})),
+			NNAInfo: {
+				__typeName: 'NNAInfo',
+				__typeValue: this.NNAInfo
+			},
+			presence: {
+				__typeName: 'NintendoPresenceV2',
+				__typeValue: this.presence
+			},
+			statusMessage: {
+				__typeName: 'Comment',
+				__typeValue: this.statusMessage
+			},
+			becameFriend: {
+				__typeName: 'DateTime',
+				__typeValue: this.becameFriend
+			},
+			lastOnline: {
+				__typeName: 'DateTime',
+				__typeValue: this.lastOnline
+			},
+			unknown: {
+				__typeName: 'uint64',
+				__typeValue: this.unknown
+			}
+		};
+	}
 }
 NEXTypes.AnyDataHolder.addType('FriendInfo', FriendInfo);
 
@@ -178,9 +414,30 @@ class FriendRequest extends NEXTypes.Structure {
 	 * @param {Stream} stream NEX data stream
 	 */
 	parse(stream) {
-		this.principalInfo = stream.readNEXStructure(PrincipalBasicInfo);
+		this.principalBasicInfo = stream.readNEXStructure(PrincipalBasicInfo);
 		this.message = stream.readNEXStructure(FriendRequestMessage);
 		this.sentOn = stream.readNEXDateTime();
+	}
+
+	toJSON() {
+		return {
+			__typeInherits: this._parentTypes.map(value => ({
+				__typeName: value.constructor.name,
+				__typeValue: value
+			})),
+			principalBasicInfo: {
+				__typeName: 'PrincipalBasicInfo',
+				__typeValue: this.principalBasicInfo
+			},
+			message: {
+				__typeName: 'FriendRequestMessage',
+				__typeValue: this.message
+			},
+			sentOn: {
+				__typeName: 'DateTime',
+				__typeValue: this.sentOn
+			}
+		};
 	}
 }
 NEXTypes.AnyDataHolder.addType('FriendRequest', FriendRequest);
@@ -207,6 +464,51 @@ class FriendRequestMessage extends NEXTypes.Structure {
 		this.unknown4 = stream.readNEXDateTime();
 		this.expiresOn = stream.readNEXDateTime();
 	}
+
+	toJSON() {
+		return {
+			__typeInherits: this._parentTypes.map(value => ({
+				__typeName: value.constructor.name,
+				__typeValue: value
+			})),
+			friendRequestId: {
+				__typeName: 'uint64',
+				__typeValue: this.friendRequestId
+			},
+			isRecieved: {
+				__typeName: 'uint8',
+				__typeValue: this.isRecieved
+			},
+			unknown1: {
+				__typeName: 'uint8',
+				__typeValue: this.unknown1
+			},
+			message: {
+				__typeName: 'String',
+				__typeValue: this.message
+			},
+			unknown2: {
+				__typeName: 'uint8',
+				__typeValue: this.unknown2
+			},
+			unknown3: {
+				__typeName: 'String',
+				__typeValue: this.unknown3
+			},
+			gameKey: {
+				__typeName: 'GameKey',
+				__typeValue: this.gameKey
+			},
+			unknown4: {
+				__typeName: 'DateTime',
+				__typeValue: this.unknown4
+			},
+			expiresOn: {
+				__typeName: 'DateTime',
+				__typeValue: this.expiresOn
+			}
+		};
+	}
 }
 
 class BlacklistedPrincipal extends NEXTypes.Structure {
@@ -224,6 +526,27 @@ class BlacklistedPrincipal extends NEXTypes.Structure {
 		this.principalBasicInfo = stream.readNEXStructure(PrincipalBasicInfo);
 		this.gameKey = stream.readNEXStructure(GameKey);
 		this.blacklistedSince = stream.readNEXDateTime();
+	}
+
+	toJSON() {
+		return {
+			__typeInherits: this._parentTypes.map(value => ({
+				__typeName: value.constructor.name,
+				__typeValue: value
+			})),
+			principalBasicInfo: {
+				__typeName: 'PrincipalBasicInfo',
+				__typeValue: this.principalBasicInfo
+			},
+			gameKey: {
+				__typeName: 'GameKey',
+				__typeValue: this.gameKey
+			},
+			blacklistedSince: {
+				__typeName: 'DateTime',
+				__typeValue: this.blacklistedSince
+			}
+		};
 	}
 }
 
@@ -245,6 +568,35 @@ class PersistentNotification extends NEXTypes.Structure {
 		this.unknown4 = stream.readUInt32LE();
 		this.unknown5 = stream.readNEXString();
 	}
+
+	toJSON() {
+		return {
+			__typeInherits: this._parentTypes.map(value => ({
+				__typeName: value.constructor.name,
+				__typeValue: value
+			})),
+			unknown1: {
+				__typeName: 'uint64',
+				__typeValue: this.unknown1
+			},
+			unknown2: {
+				__typeName: 'uint32',
+				__typeValue: this.unknown2
+			},
+			unknown3: {
+				__typeName: 'uint32',
+				__typeValue: this.unknown3
+			},
+			unknown4: {
+				__typeName: 'uint32',
+				__typeValue: this.unknown4
+			},
+			unknown5: {
+				__typeName: 'String',
+				__typeValue: this.unknown5
+			}
+		};
+	}
 }
 
 class PersistentNotificationList extends NEXTypes.Structure {
@@ -260,6 +612,19 @@ class PersistentNotificationList extends NEXTypes.Structure {
 	 */
 	parse(stream) {
 		this.notifications = stream.readNEXString(PersistentNotification);
+	}
+
+	toJSON() {
+		return {
+			__typeInherits: this._parentTypes.map(value => ({
+				__typeName: value.constructor.name,
+				__typeValue: value
+			})),
+			notifications: {
+				__typeName: 'List<PersistentNotification>',
+				__typeValue: this.notifications
+			}
+		};
 	}
 }
 NEXTypes.AnyDataHolder.addType('PersistentNotificationList', PersistentNotificationList);
