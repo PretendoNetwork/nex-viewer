a72d03072a4b2da276d6af03cf29169eaaa56306
Daniel López Guimaraes <danielectra@outlook.com>
2023-04-04 23:19:28 +0100

    Split missing NEX protocols
    These protocols can have different versions, so split them too. Also
    move the NEX title files to the nex directory, to keep the root clean

--- build_games_list.js
+++ build_games_list.js
@@ -1,13 +1,20 @@
 const fs = require('fs');
 
-const nex = fs.readFileSync('./nex.txt').toString(); // Data from https://kinnay.github.io/view.html?page=wiiu
-const nexMatchMaking = fs.readFileSync('./nex_match_making.txt').toString();
-const nexDataStore = fs.readFileSync('./nex_datastore.txt').toString();
-const access = fs.readFileSync('./access.txt').toString(); // Data from https://kinnay.github.io/view.html?page=nexwiiu
+// Data from https://kinnay.github.io/view.html?page=wiiu
+const nex = fs.readFileSync('nex/nex.txt').toString();
+const nexRanking = fs.readFileSync('nex/nex_ranking.txt').toString();
+const nexDataStore = fs.readFileSync('nex/nex_datastore.txt').toString();
+const nexMatchMaking = fs.readFileSync('nex/nex_match_making.txt').toString();
+const nexMessaging = fs.readFileSync('nex/nex_messaging.txt').toString();
+const nexUtility = fs.readFileSync('nex/nex_utility.txt').toString();
+const access = fs.readFileSync('./access.txt').toString();
 
 const nexLines = nex.split('\n');
-const nexMatchMakingLines = nexMatchMaking.split('\n');
+const nexRankingLines = nexRanking.split('\n');
 const nexDataStoreLines = nexDataStore.split('\n');
+const nexMatchMakingLines = nexMatchMaking.split('\n');
+const nexMessagingLines = nexMessaging.split('\n');
+const nexUtilityLines = nexUtility.split('\n');
 const accessLines = access.split('\n');
 
 // * Start the list with the Friends server
@@ -44,8 +51,8 @@ for (const line of nexLines) {
 	});
 }
 
-// Fill in title NEX additional Matchmaking versions
-for (const line of nexMatchMakingLines) {
+// Fill in title NEX additional Ranking versions
+for (const line of nexRankingLines) {
 	// Discard empty lines
 	if (!line) continue;
 
@@ -55,7 +62,7 @@ for (const line of nexMatchMakingLines) {
 	const game = titles.find(game => game.name === name.trim());
 
 	if (game) {
-		game.nex_match_making_version = {
+		game.nex_ranking_version = {
 			major: Number(major),
 			minor: Number(minor),
 			patch: Number(patch)
@@ -69,7 +76,7 @@ for (const line of nexMatchMakingLines) {
 				minor: 0,
 				patch: 0
 			},
-			nex_match_making_version: {
+			nex_ranking_version: {
 				major: Number(major),
 				minor: Number(minor),
 				patch: Number(patch)
@@ -104,6 +111,108 @@ for (const line of nexDataStoreLines) {
 				patch: 0
 			},
 			nex_datastore_version: {
+				major: Number(major),
+				minor: Number(minor),
+				patch: Number(patch)
+			}
+		});
+	}
+}
+
+// Fill in title NEX additional Matchmaking versions
+for (const line of nexMatchMakingLines) {
+	// Discard empty lines
+	if (!line) continue;
+
+	const [name, nexVersion] = line.split('|');
+	const [major, minor, patch] = nexVersion.trim().split('.');
+
+	const game = titles.find(game => game.name === name.trim());
+
+	if (game) {
+		game.nex_match_making_version = {
+			major: Number(major),
+			minor: Number(minor),
+			patch: Number(patch)
+		};
+	} else {
+		titles.push({
+			name: name.trim(),
+			access_key: '',
+			nex_version: {
+				major: 0,
+				minor: 0,
+				patch: 0
+			},
+			nex_match_making_version: {
+				major: Number(major),
+				minor: Number(minor),
+				patch: Number(patch)
+			}
+		});
+	}
+}
+
+// Fill in title NEX additional Messaging versions
+for (const line of nexMessagingLines) {
+	// Discard empty lines
+	if (!line) continue;
+
+	const [name, nexVersion] = line.split('|');
+	const [major, minor, patch] = nexVersion.trim().split('.');
+
+	const game = titles.find(game => game.name === name.trim());
+
+	if (game) {
+		game.nex_messaging_version = {
+			major: Number(major),
+			minor: Number(minor),
+			patch: Number(patch)
+		};
+	} else {
+		titles.push({
+			name: name.trim(),
+			access_key: '',
+			nex_version: {
+				major: 0,
+				minor: 0,
+				patch: 0
+			},
+			nex_messaging_version: {
+				major: Number(major),
+				minor: Number(minor),
+				patch: Number(patch)
+			}
+		});
+	}
+}
+
+// Fill in title NEX additional Utility versions
+for (const line of nexUtilityLines) {
+	// Discard empty lines
+	if (!line) continue;
+
+	const [name, nexVersion] = line.split('|');
+	const [major, minor, patch] = nexVersion.trim().split('.');
+
+	const game = titles.find(game => game.name === name.trim());
+
+	if (game) {
+		game.nex_utility_version = {
+			major: Number(major),
+			minor: Number(minor),
+			patch: Number(patch)
+		};
+	} else {
+		titles.push({
+			name: name.trim(),
+			access_key: '',
+			nex_version: {
+				major: 0,
+				minor: 0,
+				patch: 0
+			},
+			nex_utility_version: {
 				major: Number(major),
 				minor: Number(minor),
 				patch: Number(patch)
