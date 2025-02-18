450871f40b7e3bf27ce55120b8cbafede901a40e
TraceEntertains <atw3710@gmail.com>
2025-02-18 17:24:36 -0500

    Hopefully(?) fix copy paste on macos

--- src/windows/main/menu.ts
+++ src/windows/main/menu.ts
@@ -92,6 +92,18 @@ export default function createMenu(): Menu {
 			]
 		},
 		{
+			label: "Edit",
+			submenu: [
+				{ label: "Undo", accelerator: "CmdOrCtrl+Z" },
+				{ label: "Redo", accelerator: "Shift+CmdOrCtrl+Z" },
+				{ type: "separator" },
+				{ label: "Cut", accelerator: "CmdOrCtrl+X" },
+				{ label: "Copy", accelerator: "CmdOrCtrl+C" },
+				{ label: "Paste", accelerator: "CmdOrCtrl+V" },
+				{ label: "Select All", accelerator: "CmdOrCtrl+A" }
+			]
+		},
+		{
 			label: 'Options',
 			id: 'options',
 			submenu: [
